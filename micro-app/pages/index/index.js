923f461de8fe03303a53d5ee6056a9430e4e650a
Hstarorg <hm910705@163.com>
2018-02-05 18:19:02 +0800

    Add moto add page

---
+++ micro-app/pages/index/index.js
@@ -0,0 +1,37 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'My god',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  //事件处理函数
+  handleBtnAddTap: function () {
+    wx.navigateTo({
+      url: '../moto-add/moto-add'
+    })
+  },
+})
