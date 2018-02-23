85c1db2310fa02889b474585b154231a7dbfaf97
Hstarorg <hm910705@163.com>
2018-02-23 23:55:15 +0800

    UI update

--- micro-app/app.js
+++ micro-app/app.js
@@ -7,33 +7,44 @@ App({
     wx.login({
       success: res => {
         this.globalData.code = res.code;
+        const self = this;
         // 获取用户信息
         wx.getSetting({
           success: res => {
             if (res.authSetting['scope.userInfo']) {
-              // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-              wx.getUserInfo({
-                success: res => {
-                  // 可以将 res 发送给后台解码出 unionId
-                  this.globalData.userInfo = res.userInfo;
-                  const postData = {
-                    encryptedData: res.encryptedData,
-                    iv: res.iv,
-                    code: this.globalData.code
-                  };
-                  ajax.post(`${config.apiHost}/account/token`, postData)
-                    .then(({ data }) => {
-                      this.globalData.token = data.token;
-                      ajax.setToken(data.token);
-                      if (this.userInfoReadyCallback) {
-                        this.userInfoReadyCallback(res);
-                      }
-                    });
+              self.doLogin();
+            } else {
+              wx.authorize({
+                scope: 'scope.userInfo',
+                success() {
+                  self.doLogin();
                 }
               })
             }
           }
         })
+      }
+    })
+  },
+  doLogin() {
+    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    wx.getUserInfo({
+      success: res => {
+        // 可以将 res 发送给后台解码出 unionId
+        this.globalData.userInfo = res.userInfo;
+        const postData = {
+          encryptedData: res.encryptedData,
+          iv: res.iv,
+          code: this.globalData.code
+        };
+        ajax.post(`${config.apiHost}/account/token`, postData)
+          .then(({ data }) => {
+            this.globalData.token = data.token;
+            ajax.setToken(data.token);
+            if (this.userInfoReadyCallback) {
+              this.userInfoReadyCallback(res);
+            }
+          });
       }
     })
   },
