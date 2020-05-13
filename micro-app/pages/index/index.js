0a8bd06e0992d0ad6b9ea5acc1234f8c673a5f2c
hstarorg <hm910705@163.com>
2020-05-14 02:15:19 +0800

    feat: add tabbar

--- micro-app/pages/index/index.js
+++ micro-app/pages/index/index.js
@@ -10,30 +10,30 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    motoList: []
+    motoList: [],
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
       });
       this._loadUserMotoList();
     } else if (this.data.canIUse) {
-      app.userInfoReadyCallback = res => {
+      app.userInfoReadyCallback = (res) => {
         this._loadUserMotoList();
       };
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
-        success: res => {
+        success: (res) => {
           app.globalData.userInfo = res.userInfo;
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+            hasUserInfo: true,
+          });
+        },
+      });
     }
   },
   onReady() {
@@ -42,20 +42,19 @@ Page({
   //事件处理函数
   handleBtnAddTap: function () {
     wx.navigateTo({
-      url: '../moto-add/moto-add'
+      url: '../moto-add/moto-add',
     });
   },
   navigateToFuelList(e) {
     const motoId = e.currentTarget.dataset.motoId;
     wx.navigateTo({
-      url: `../fuel-list/fuel-list?motoId=${motoId}`
+      url: `../fuel-list/fuel-list?motoId=${motoId}`,
     });
   },
   _loadUserMotoList() {
-    ajax.get(`${config.apiHost}/motos`)
-      .then(({ data }) => {
-        wx.hideLoading();
-        this.setData({ motoList: data });
-      });
-  }
-})
+    ajax.get(`${config.apiHost}/motos`).then(({ data }) => {
+      wx.hideLoading();
+      this.setData({ motoList: data });
+    });
+  },
+});
